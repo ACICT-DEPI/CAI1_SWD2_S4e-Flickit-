dbdc49090ceea8012004d32ff5bf149de7195ac4
Nouran Ahmed <ahmednoran200475@gmail.com>
2024-10-02 22:29:35 +0300

    play several rounds in One gamePlay, end-game button, score edited

--- flickitserver/src/Utils/dbConnection.js
+++ flickitserver/src/Utils/dbConnection.js
@@ -1,7 +1,12 @@
 const mongoose = require ( "mongoose");
 
  const dbConnection=()=>{
-    mongoose.connect("mongodb+srv://nora:Flickit@cluster0.ptpifd2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    // mongoose.connect("mongodb+srv://nora:Flickit@cluster0.ptpifd2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    //     console.log("database connected");
+    // }).catch((err)=>{
+    //     console.log("error in connect");
+    // })
+    mongoose.connect("mongodb+srv://testdatabase:mongodbfortest@cluster0.cewkt.mongodb.net/").then(()=>{
         console.log("database connected");
     }).catch((err)=>{
         console.log("error in connect");
